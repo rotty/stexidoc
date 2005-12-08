75e1400709b4cb3721496de71039e967afbb18f0
a.rottmann <a.rottmann@gmx.at>
2005-12-08 23:13:37 +0100

    Removed bit-rotted testsuite
    darcs-hash:20051208221337-1cc0d-a64db87130af774a2956e90a1d398c824c1bb277.gz

--- tests/basic.scm
+++
@@ -1,9 +0,0 @@
-(let ((code ";; a comment\n(define (foo x) (* x 2))"))
-  (testeez
-   "basic"
-   (test/equal
-    "simple"
-    (sml->scheme (scheme->sml (open-input-string code))) code)))
-
-
-;; arch-tag: eb4b0913-8337-4c7c-bc02-ecee372b51af
--- tests/tests.scm
+++
@@ -1,3 +0,0 @@
-("basic.scm")
-
-;; arch-tag: f0d2d231-6724-4ba1-a1a9-658c8fb4bf26
