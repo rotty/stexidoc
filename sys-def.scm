b832a17fb44d4d556ad04b5e313a30c6442f7d63
Andreas Rottmann <rotty@debian.org>
2006-01-18 14:13:33 +0100

    Get rid of system version (for now)
    darcs-hash:20060118131333-b4420-d403791dde0fb113f77170b6750929a523c23706.gz

--- sys-def.scm
+++ sys-def.scm
@@ -1,3 +1,3 @@
-(define-system stexidoc (0 1 0)
+(define-system stexidoc
   (dependencies spells sxml texinfo)
   (config-files packages))
--- tests/systems/foo/sys-def.scm
+++ tests/systems/foo/sys-def.scm
@@ -1,10 +1,10 @@
 ;; @subsection Metasyntactics
 
 ;;@ Contains facilities to ensure fooish behaviour
-(define-system foo (0 1 0)
+(define-system foo
   (spedl-files packages))
 
 ;; @subsection Blah
 
 ;;@ Blah, blah...
-(define-system blah (0 1 0))
+(define-system blah)
